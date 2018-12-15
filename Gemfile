6d5732ef55b477175b6700fc02b13af953a78caf
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-15 20:56:08 +0100

    rubocop refactor

--- Gemfile
+++ Gemfile
@@ -2,17 +2,17 @@ source 'https://rubygems.org'
 
 ruby '2.4.0'
 
-gem 'rake'
-gem 'hanami',       '~> 1.3'
+gem 'hanami', '~> 1.3'
 gem 'hanami-model', '~> 1.3'
+gem 'rake'
 
 gem 'pg'
 
 group :development do
   # Code reloading
   # See: http://hanamirb.org/guides/projects/code-reloading
-  gem 'shotgun', platforms: :ruby
   gem 'hanami-webconsole'
+  gem 'shotgun', platforms: :ruby
 end
 
 group :test, :development do
@@ -20,8 +20,8 @@ group :test, :development do
 end
 
 group :test do
-  gem 'rspec'
   gem 'capybara'
+  gem 'rspec'
 end
 
 group :production do
--- apps/web/application.rb
+++ apps/web/application.rb
@@ -18,9 +18,9 @@ module Web
       #
       # When you add new directories, remember to add them here.
       #
-      load_paths << [
-        'controllers',
-        'views'
+      load_paths << %w[
+        controllers
+        views
       ]
 
       # Handle exceptions with HTTP statuses (true) or don't catch them (false).
@@ -233,7 +233,7 @@ module Web
       #
       #  * https://developer.mozilla.org/en-US/docs/Web/Security/CSP/CSP_policy_directives
       #
-      security.content_security_policy %{
+      security.content_security_policy %(
         form-action 'self';
         frame-ancestors 'self';
         base-uri 'self';
@@ -248,7 +248,7 @@ module Web
         child-src 'self';
         frame-src 'self';
         media-src 'self'
-      }
+      )
 
       ##
       # FRAMEWORKS
