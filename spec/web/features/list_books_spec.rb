edcdd98c71a9cb47a4e957db9198aa74f149210c
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-16 23:47:37 +0100

    clean up specs

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -1,9 +1,20 @@
+# spec/web/features/list_books_spec.rb
 require 'features_helper'
 
 RSpec.describe 'List books' do
+  let(:repository) { BookRepository.new }
+  before do
+    repository.clear
+
+    repository.create(title: 'PoEAA', author: 'Martin Fowler')
+    repository.create(title: 'TDD',   author: 'Kent Beck')
+  end
+
   it 'displays each book on the page' do
     visit '/books'
 
-    expect(page).to have_css('.book', count: 2)
+    within '#books' do
+      expect(page).to have_selector('.book', count: 2), 'Expected to find 2 books'
+    end
   end
 end
--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -1,10 +1,37 @@
 RSpec.describe Web::Views::Books::Index, type: :view do
-  let(:exposures) { Hash[format: :html] }
+  let(:exposures) { Hash[format: :html, books: []] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
   it 'exposes #format' do
     expect(view.format).to eq exposures.fetch(:format)
+  end
+  
+  it 'exposes #books' do
+    expect(view.books).to eq exposures.fetch(:books)
+  end
+
+  context 'when there are no books' do
+    it 'renders placeholder message' do
+      placeholder = '<p class="placeholder">There are no books yet.</p>'
+      expect(rendered).to include(placeholder)
+    end
+  end
+
+  context 'when there are books' do
+    let(:book1)     { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
+    let(:book2)     { Book.new(title: 'Domain Driven Design', author: 'Eric Evans') }
+    let(:exposures) { Hash[books: [book1, book2]] }
+
+    it 'lists them all' do
+      expect(rendered.scan(/class="book"/).length).to eq(2)
+      expect(rendered).to include('Refactoring')
+      expect(rendered).to include('Domain Driven Design')
+    end
+
+    it 'hides the placeholder message' do
+      expect(rendered).to_not include('<p class="placeholder">There are no books yet.</p>')
+    end
   end
 end
