08eadaf38466546f459a1c212a4827f0bc3aa587
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-17 21:33:35 +0100

    use route helpers

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 root to: 'home#index'
-
-get '/books', to: 'books#index'
-get '/books/new', to: 'books#new'
-post '/books', to: 'books#create'
+resources :books, only: [:index, :new, :create]
