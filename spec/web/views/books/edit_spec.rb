22a126492b170dfe119b85a3ba2c18aa51918da9
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-19 08:40:12 +0100

    edit validations specs

---
+++ spec/web/views/books/edit_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe Web::Views::Books::Edit, type: :view do
+  let(:params) do
+    OpenStruct.new(
+      valid?: false, error_messages: ['Title must be filled']
+    )
+  end
+  let(:book) { BookRepository.new.create(title: 'Broomhilda', author: 'Otto') }
+  let(:exposures) { Hash[format: :html, params: params, book: book] }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/books/edit.html.erb') }
+  let(:view)      { described_class.new(template, exposures) }
+  let(:rendered)  { view.render }
+
+  it 'displays the value of fields already entered' do
+    expect(rendered).to include('Otto')
+  end
+
+  it 'displays list of errors when params contains errors' do
+    expect(rendered).to include('There was a problem with your submission')
+    expect(rendered).to include('Title must be filled')
+  end
+end
