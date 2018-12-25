c9766609d60998d80ce0046fd5549d61e292d09b
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-25 07:15:01 +0100

    dummy for authenticate callback

--- apps/web/controllers/books/index.rb
+++ apps/web/controllers/books/index.rb
@@ -5,11 +5,24 @@ module Web
     module Books
       class Index
         include Web::Action
-
+        before :authenticate!
         expose :books
 
         def call(_params)
           @books = BookRepository.new.all
+        end
+
+        private
+
+        def authenticate!
+          return if signed_in?
+
+          flash[:error] = 'Please sign in first'
+          redirect_to routes.new_registration_path
+        end
+
+        def signed_in?
+          !session[:user_id].nil?
         end
       end
     end
