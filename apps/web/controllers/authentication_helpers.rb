52b0a2a284c5dabc2341f3df60b804a6b1c66278
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-25 07:25:36 +0100

    extract authentication helpers to separate module

---
+++ apps/web/controllers/authentication_helpers.rb
@@ -0,0 +1,18 @@
+module Web
+  module Controllers
+    module AuthenticationHelpers
+      private
+
+      def authenticate!
+        return if signed_in?
+
+        flash[:error] = 'Please sign in first'
+        redirect_to routes.new_registration_path
+      end
+
+      def signed_in?
+        !session[:user_id].nil?
+      end
+    end
+  end
+end
--- apps/web/controllers/books/index.rb
+++ apps/web/controllers/books/index.rb
@@ -5,24 +5,13 @@ module Web
     module Books
       class Index
         include Web::Action
+        include Web::Controllers::AuthenticationHelpers
+
         before :authenticate!
         expose :books
 
         def call(_params)
           @books = BookRepository.new.all
-        end
-
-        private
-
-        def authenticate!
-          return if signed_in?
-
-          flash[:error] = 'Please sign in first'
-          redirect_to routes.new_registration_path
-        end
-
-        def signed_in?
-          !session[:user_id].nil?
         end
       end
     end
