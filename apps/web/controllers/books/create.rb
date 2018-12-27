e23207a079387cead5e6e5beecc85d2b2d53cc97
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-27 07:07:34 +0100

    add user id params validation

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -5,11 +5,13 @@ module Web
     module Books
       class Create
         include Web::Action
+        include Web::Controllers::AuthenticationHelpers
 
         params do
           required(:book).schema do
-            required(:title).filled(:str?)
             required(:author).filled(:str?)
+            required(:title).filled(:str?)
+            required(:user_id).filled(:int?)
             optional(:description).filled(:str?)
           end
         end
