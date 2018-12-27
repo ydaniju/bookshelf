127a54b6ed4f03ed5040879cde7b515c7ad54cb6
Yusuf Daniju <danijuyusuf@outlook.com>
2018-12-27 06:44:13 +0100

    reshape database information

--- db/migrations/20181224111721_add_description_to_books.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Hanami::Model.migration do
-  change do
-    add_column :books, :description, String
-  end
-end
--- db/migrations/20181226221240_create_books.rb
+++ db/migrations/20181226221240_create_books.rb
@@ -5,8 +5,11 @@ Hanami::Model.migration do
     create_table :books do
       primary_key :id
 
+      foreign_key :user_id, :users, on_delete: :cascade, null: false
+
       column :title, String, null: false
       column :author, String, null: false
+      column :description, String
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
